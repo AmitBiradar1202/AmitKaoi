5cbfc7b335fe5a2cffe9a9d085094f1e3b1fc869
AmitBiradar1202 <amitbiradar1202@gmail.com>
2025-11-06 14:45:03 +0530

    app.jsx update

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -12,33 +12,30 @@ import Orders from './pages/Orders'
 import Verify from './pages/Verify'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
-import { ToastContainer, toast } from 'react-toastify';
-
+import { ToastContainer } from 'react-toastify';
 import SearchBar from './components/SearchBar'
-const App = () => {
+import ScrollToTop from './components/ScrollToTop'
 
+const App = () => {
   return (
     <div className='px-4 sm:px-[5vw] md:px[7vw] lg:px[9vw]'>
       <ToastContainer />
-      <Navbar></Navbar>
+      <Navbar />
       <SearchBar />
+      <ScrollToTop /> {/* Add this here */}
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/collection' element={<Collection />} />
         <Route path='/about' element={<About />} />
         <Route path='/contact' element={<Contact />} />
-
         <Route path='/product/:productId' element={<Product />} />
         <Route path='/cart' element={<Cart />} />
         <Route path='/login' element={<Login />} />
-
         <Route path='/place-order' element={<PlaceOrder />} />
         <Route path='/orders' element={<Orders />} />
         <Route path='/verify' element={<Verify />} />
-
-
       </Routes>
-      <Footer></Footer>
+      <Footer />
     </div>
   )
 }
